e2e2219baa0355b0d0d7b3a1cc457426eb851a3d
Nirahulan <60203633+Nirahulan@users.noreply.github.com>
2021-02-16 00:11:05 -0500

    project plan

--- app/src/main/java/com/myappcompany/nira/guessthecelebrity/MainActivity.java
+++ app/src/main/java/com/myappcompany/nira/guessthecelebrity/MainActivity.java
@@ -1,11 +1,17 @@
 package com.myappcompany.nira.guessthecelebrity;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,10 +40,22 @@ public class MainActivity extends AppCompatActivity {
             imageView = findViewById(R.id.imageview);//add imageview in view
     }
 
-    public class ImageDownloader extends AsyncTask<String, void, Bitmap>{
+    public class ImageDownloader extends AsyncTask<String, Void, Bitmap>{
 
         @Override
-        protected Bitmap doInBackground(String... strings) {
+        protected Bitmap doInBackground(String... urls) {
+            try{
+                URL url = new URL(urls[0]);
+                HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+                connection.connect();
+                InputStream in = connection.getInputStream();
+                Bitmap myBitmap = BitmapFactory.decodeStream(in);
+                return myBitmap;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
             return null;
         }
     }
